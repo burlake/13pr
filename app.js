e17ba319e80321fa81497381ef97b85fc8f61881
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-11-15 22:59:09 +0300

    1

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { PORT = 3000} = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/testdb' } = process.env;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.listen(PORT, () => {
-  console.log ('есть порт 3000')
+  console.log('есть порт 3000');
 });
