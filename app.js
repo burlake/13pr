fd8b17c420d1d8dff260160f7cf6414a25cea5d3
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-11-15 22:19:02 +0300

    1

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const { PORT = 3000} = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log ('есть порт 3000')
+});
