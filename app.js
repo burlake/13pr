73444fc329bf959dde4ae620b1170ce15ce03eb6
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-11-28 22:40:21 +0300

    1

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/testdb' } = process.env;
-
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
