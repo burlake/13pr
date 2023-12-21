5fab69f904a5c9338c818b64cdf6dc78c9a350f2
Elizaveta Burlakova <es.burlakova@mail.ru>
2023-12-21 20:45:43 +0300

    1

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/testdb' } = process.env;
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,4 +16,8 @@ mongoose.connect(DB_URL, {
 
 app.use('/users', require('./routes/users'));
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+
--- controllers/users.js
+++ controllers/users.js
@@ -3,22 +3,22 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
-module.exports.getUsersById = (req, res) => {
+module.exports.getUserById = (req, res) => {
   if (req.params.userId.length === 24) {
     User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        res.status(404).send({message: 'Произошла ошибка. Пользователь с ID не найден'});
+        res.status(404).send({message: 'Произошла ошибка. Пользователь с id не найден'});
         return;
       }
       res.send(user);
     })
-    .catch(() => res.status(404).send({message: 'Произошла ошибка. Пользователь с ID не найден'}));
+    .catch(() => res.status(404).send({message: 'Произошла ошибка. Пользователь с id не найден'}));
   } else {
-    res.status(400).send({message: 'Произошла ошибка. ID некорректный'});
+    res.status(400).send({message: 'Произошла ошибка. id некорректный'});
   }
 };
 
@@ -33,4 +33,37 @@ module.exports.addUser = (req, res) => {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
     });
+};
+
+module.exports.editUserData = (req, res) => {
+  const { name, about } = req.body;
+  if (req.user._id) {
+    User.findByIdAndUpdate (req.user._id, { name, about }, { new: 'true', runValidators: true})
+    .then ((user) => res.send(user))
+    .catch ((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send ({ message: err.message });
+      } else {
+        res.status(404).send ({message: 'Произошла ошибка. Пользователь с id не найден'});
+      }
+    });
+  } else {
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
+};
+
+module.exports.editUserAvatar = (req, res) => {
+  if (req.user._id) {
+    User.findByIdAndUpdate(req.user._id, {avatar: req.body.avatar}, {new: 'true', runValidators: true})
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send ({message: err.message});
+      } else {
+        res.status(404).send ({ message: 'Произошла ошибка. Пользователь с id не найден' });
+      }
+    });
+  } else {
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
 };
